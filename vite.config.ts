1bfd9c36112bd1fe497f44c5792289d498e8654e
dimon9995 <mikhailov1500@mail.ru>
2026-05-09 16:52:56 +0300

    updated pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html"
+    }
   }
 });
